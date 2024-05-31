a94fec27b140f65a691210e683e7b9eafa320662
Fra <fra24eso@gmail.com>
2024-05-31 21:14:38 +0200

    install & setting

--- src/views/GetStartView.vue
+++ src/views/GetStartView.vue
@@ -33,21 +33,21 @@ export default {
     <div id="my_proj" class="container p-5">
         <div class="row align-items-center justify-content-center">
             <div class="col-lg-12 text-center">
-                <h1 class="txt_title">Tutorials</h1>
+                <h1 class="txt_title_thin">Tutorials</h1>
                 <p class="txt_proj">
-                    Click on a tutorials, go to the summary and view the video
+                    Click on and go to the summary and video tutorials
                 </p>
-                <div class="row py-5 justify-content-center">
-                    <div :to="`/project/${project.id}`" class="col-lg-8 col-md-4 col-sm-6" v-for="project in projects"
+                <div class="row justify-content-center">
+                    <div :to="`/project/${project.id}`" class="col-12 col-lg-6 col-md-4" v-for="project in projects"
                         :key="project.id" @click="router.push(`/project/${project.id}`)">
-                        <div class="card rounded-5 my-4">
-                            <img :src="getImagePath(project.image)" alt="project image" class="img_cards rounded-top-5"/>
+                        <div class="card rounded-5 my-4 bg-dark text_cards">
                             <div class="card-body">
-                                <h4 class="card-title">{{ project.tutorial }}</h4>
+                                <h5 class="text-center">{{ project.num_tutorial }}</h5>
                                 <h1 class="card-title">{{ project.name }}</h1>
-                                <!-- <p class="card-text">Click on</p> -->
                                 <p class="card-text">{{ project.desc_card }}</p>
                             </div>
+                            <img :src="getImagePath(project.image)" alt="project image"
+                                class="img_cards rounded-bottom-5" />
                         </div>
 
                     </div>
@@ -61,7 +61,10 @@ export default {
 </template>
 
 <style scoped>
-.img_cards{
+.img_cards {
     width: 100%;
+}
+.text_cards{
+    color:rgb(185, 253, 255)
 }
 </style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -26,19 +26,22 @@ export default {
   <div class="container p-5">
     <div class="row align-items-center justify-content-center">
       <div class="col-lg-12">
-        <h1 class="text-center">{{ project.name }}</h1>
-        <p class="txt_proj" v-html="project.desc_tutorial"></p>
+        <h1 class="text-center mb-5">{{ project.name }}</h1>
+        <p class="text-center">{{ project.num_tutorial }}</p>
+        <p class="" v-html="project.recommendations"></p>
+        <p class="" v-html="project.aims_tutorial"></p>
         <div class="row py-5 justify-content-center">
-          <div class="col-lg-3 col-md-4 col-sm-6" v-for="tutorial in project.tutorials" :key="tutorial.id">
-            <div class="card text-left rounded-5">
+          <div class="col-12 col-lg-4 col-md-4 col-sm-6 mb-5" v-for="tutorial in project.tutorials" :key="tutorial.id">
+            <div class="card text-left rounded-5 bg-dark text_cards">
               <img :src="getImagePath(tutorial.img_tutorial)" alt="tutorial image" class="img_cards rounded-top-5" />
               <div class="card-body">
-                <a :href="tutorial.video_url" target="_blank" class="btn btn-primary">Watch Video</a>
-                <h4 class="card-title">Title</h4>
-                <p class="card-text">Body</p>
+                <h4 class="card-title">{{tutorial.title}}</h4>
+                <a :href="tutorial.video_url" target="_blank" class="btn btn-danger">Watch Video</a>
+                <!-- <p class="card-text">Body</p> -->
               </div>
-            </div>
+            </div> 
           </div>
+          
         </div>
       </div>
     </div>
@@ -48,5 +51,8 @@ export default {
 <style scoped>
 .img_cards{
     width: 100%;
+}
+.text_cards{
+    color:rgb(185, 253, 255)
 }
 </style>
